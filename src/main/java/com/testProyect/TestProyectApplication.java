042ce0b38432ae0f360d3752b5111370b3faebee
DESKTOP-ODJJEUE\Hellen <hellen.campos@avantica.net>
2017-04-27 14:47:43 -0600

    no message

---
+++ src/main/java/com/testProyect/TestProyectApplication.java
@@ -0,0 +1,12 @@
+package com.testProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProyectApplication.class, args);
+	}
+}
