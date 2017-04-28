a0ac72b7da1aae7eb2d5037c9cf02d23b4b00320
AVANTEK-SC\hcampos-as <hellen.campos@avantica.net>
2017-04-28 17:04:45 -0600

    no message

--- src/main/java/com/javaWebServices/config/BeansContext.java
+++ src/main/java/com/javaWebServices/config/BeansContext.java
@@ -1,5 +1,13 @@
 package com.javaWebServices.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+@EnableJpaRepositories
 public class BeansContext {
 
+	
 }
